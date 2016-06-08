8b87cfc4117a58199ada634738871223b548c8cf
XuChang <chinawraith@163.com>
2016-06-08 18:34:33 +0800

    完成了mysql上的select，以及mysql&mongo上的CUD

--- test/def/dataSource.js
+++ test/def/dataSource.js
@@ -8,7 +8,10 @@ const dataSources = {
         type: "mysql",
         url: "mysql://root@localhost:3306/testorm",
         settings: {
-            "connection.pool": true
+            "connection.pool": true,
+            disableUpdateAt: false,
+            disableCreateAt: false,
+            disableDeleteAt: false                                                  //  这三个值决定了在表中是否有相关动作的时间列。如果disableDeleteAt被设置为true，则删除是真的从数据库中删除
         }
     },
     mongodb: {
